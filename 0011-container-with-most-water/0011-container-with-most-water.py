5062a6ab76c5693fae36e6376fc185f63d3f54ea
Bharath Kumar <bharath.karri23@gmail.com>
2026-07-11 01:40:59 +0530

    Time: 65 ms (14.87%), Space: 29.8 MB (5.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,14 +3,13 @@ class Solution:
         cap = 0
         k = 0
         j = len(height) - 1
-        for i in range(len(height)-1):
+        for i in range(len(height)):
             width = j - k
-            H = min(height[k] , height[j])
-            total = H * width
-            cap = max(cap,total)
+            H = min(height[k],height[j])
+            total = width * H
+            cap = max(total,cap)
             if height[k] < height[j]:
                 k += 1
             else:
                 j -= 1
         return cap
-        
